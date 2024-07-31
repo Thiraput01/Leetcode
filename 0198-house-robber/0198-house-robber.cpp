179523bee4e466848e44f16856a22889fb2f47f0
Ball <142081561+Thiraput01@users.noreply.github.com>
2024-07-31 21:39:21 +0700

    Time: 0 ms (100%), Space: 10.2 MB (6.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // Create a memoization map to store already computed results
-        std::unordered_map<int, int> memo;
+        unordered_map<int, int> memo;
         return recur(nums, nums.size()-1, memo);
     }
 
-    int recur(vector<int> &nums, int n, std::unordered_map<int, int>& memo) {
+    int recur(vector<int> &nums, int n, unordered_map<int, int>& memo) {
         if (n < 0) return 0;
 
-        // Check if the result for the current index is already computed
-        if (memo.find(n) != memo.end()) {
-            return memo[n];
-        }
-
-        // Compute the result using memoization
+        if (memo.find(n) != memo.end()) return memo[n];
+        
         int result = max(recur(nums, n-2, memo) + nums[n], recur(nums, n-1, memo));
 
-        // Store the result in the memoization map
+
         memo[n] = result;
 
         return result;
