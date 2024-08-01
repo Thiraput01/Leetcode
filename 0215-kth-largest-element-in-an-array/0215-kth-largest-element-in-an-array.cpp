3d6afa215560a7f759f41cd2fb2cf44ed67bdd3c
Ball <142081561+Thiraput01@users.noreply.github.com>
2024-08-01 20:17:41 +0700

    Time: 77 ms (90.3%), Space: 64.5 MB (9.29%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         priority_queue<int> pq;
-        for(auto &i: nums){
-            pq.push(i);
-        }
+        for(auto &i: nums) pq.push(i);
+        
         int out;
         for(int i=0; i<k; i++){
             out = pq.top();
